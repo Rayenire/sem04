534ddffccded33cbc73fbbd41c51e763a5232a78
Rayenire <aeristarg@gmail.com>
2022-07-04 11:38:45 +0300

    add task04 not ready

---
+++ task04/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Загадай число от 1 до 100");
+int n_first = 0, n_last = 100, mid;
+while (n_first <= n_last)
+{
+    mid = (n_first + n_last) / 2;
+    Console.WriteLine("Число больше {0}?", mid);
+    string ?ans = Console.ReadLine();
+    if (ans == "yes"){
+        n_first = mid;
+    }
+    else{
+        n_last = mid;
+    }
+}
+Console.WriteLine(mid);
