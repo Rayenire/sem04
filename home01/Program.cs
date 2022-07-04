08d1a788e03e8fd04d1e8bc91bfc1c22f4274d15
Rayenire <aeristarg@gmail.com>
2022-07-04 12:59:12 +0300

    add home01

---
+++ home01/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+double a, b, result;
+a = new Random().Next(0, 6);;
+Console.WriteLine(a);
+b = new Random().Next(0, 6);;
+Console.WriteLine(b);
+result = Math.Pow(a, b);
+Console.WriteLine(result);
